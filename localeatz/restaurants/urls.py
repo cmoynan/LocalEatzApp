cb2de5abf820bc4ca08f82969dcf165c6897d6b6
Chris Moynan <Chrismoynan@outlook.com>
2024-07-22 17:21:31 +0000

    Fix view function reference for home page and update URL configurations

---
+++ localeatz/restaurants/urls.py
@@ -0,0 +1,7 @@
+# restaurants/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
--- localeatz/restaurants/views.py
+++ localeatz/restaurants/views.py
@@ -1,3 +1,10 @@
+
 from django.shortcuts import render
+from .models import Restaurant
 
 # Create your views here.
+
+
+def home(request):
+    restaurants = Restaurant.objects.all()
+    return render(request, 'home.html', {'restaurants': restaurants})
