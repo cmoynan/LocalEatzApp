fb534c8911910671302f53b80fec42c6241a1cbc
Chris Moynan <Chrismoynan@outlook.com>
2024-07-23 15:47:31 +0000

    Fix NameError and add booking functionality

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,7 +1,9 @@
-# restaurants/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('book/<int:restaurant_id>/', views.book_table, name='book_table'),
+    path('submit_booking/<int:restaurant_id>/', views.submit_booking, name='submit_booking'),
 ]
+
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,10 +1,22 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from .models import Restaurant
-
-# Create your views here.
-
 
 def home(request):
     restaurants = Restaurant.objects.all()
     return render(request, 'home.html', {'restaurants': restaurants})
+
+def book_table(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    return render(request, 'book_table.html', {'restaurant': restaurant})
+
+def submit_booking(request, restaurant_id):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+        party_size = request.POST.get('party_size')
+        
+        
+        return HttpResponse("Booking submitted successfully!")
+    return redirect('home')  
