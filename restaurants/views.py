14010150cbdec9ce7d1b4462e8de778d431db9ee
Chris Moynan <Chrismoynan@outlook.com>
2024-07-23 17:49:21 +0000

    Implement booking functionality and ensure bookings appear in admin

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Restaurant
+from .forms import BookingForm
 
 def home(request):
     restaurants = Restaurant.objects.all()
@@ -11,12 +12,14 @@ def book_table(request, restaurant_id):
     return render(request, 'book_table.html', {'restaurant': restaurant})
 
 def submit_booking(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
     if request.method == 'POST':
-        name = request.POST.get('name')
-        date = request.POST.get('date')
-        time = request.POST.get('time')
-        party_size = request.POST.get('party_size')
-        
-        
-        return HttpResponse("Booking submitted successfully!")
-    return redirect('home')  
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.restaurant = restaurant
+            booking.save()
+            return redirect('home')
+    else:
+        form = BookingForm()
+    return render(request, 'restaurants/booking_form.html', {'form': form, 'restaurant': restaurant})
