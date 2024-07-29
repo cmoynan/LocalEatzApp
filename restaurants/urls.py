0349bd0c48c31c550289893df6f664739c26d036
Chris Moynan <Chrismoynan@outlook.com>
2024-07-29 19:35:59 +0000

    removed cancel booking url is error. Added back in to allow users cancel bookings

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('my-bookings/', views.my_bookings, name='my_bookings'),
     path('book_table/<int:restaurant_id>/', views.book_table, name='book_table'),
     path('booking_success/', views.booking_success, name='booking_success'),
+    path('cancel-booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
 ]
 
 
